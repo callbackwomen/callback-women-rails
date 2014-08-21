f3bb30cd749e3977044d7597aff00c4ac902047a
Strand McCutchen <strand@bettermistak.es>
2014-08-21 09:22:41 -0500

    Add signout path.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   root to: "conferences#index"
 
   get 'auth/twitter/callback', to: "sessions#create"
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 end
