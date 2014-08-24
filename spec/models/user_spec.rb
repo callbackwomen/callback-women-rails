9255f9cbe488bdeedd4ead20ed0f7406f7b53796
Strand McCutchen <strand@bettermistak.es>
2014-08-24 14:45:58 -0500

    User#find_or_create_using_omniauth returns nil with bad params

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,11 @@ describe User do
   it { should validate_presence_of :name }
   it { should validate_presence_of :provider }
   it { should validate_presence_of :uid }
+
+  context ".find_or_create_using_omniauth" do
+    it "returns nil when it receives an improperly formatted hash" do
+      user = User.find_or_create_using_omniauth provider: "Zombo"
+      expect(user).to be_nil
+    end
+  end
 end
